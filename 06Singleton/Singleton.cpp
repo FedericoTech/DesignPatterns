e749ad8897c2816a3f8ff5862bc10b44ec10efb2
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2022-08-16 16:48:31 +0100

    Singleton Design Pattern

---
+++ 06Singleton/Singleton.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class GlobalClass
+{
+	int m_value;
+	static GlobalClass *s_instance;
+
+	GlobalClass(int v = 0){
+		m_value = v;
+	}
+
+	void operator = (GlobalClass const&) = delete;
+
+public:
+	int get_value(){ return m_value; }
+
+	void set_value(int v) { m_value = v;}
+
+	static GlobalClass *instance()
+	{
+		if(!s_instance){
+			s_instance = new GlobalClass;
+		}
+
+		return s_instance;
+	}
+};
+
+GlobalClass *GlobalClass::s_instance = 0;
+
+void foo(void){
+	GlobalClass::instance()->set_value(1);
+	std::cout << __FUNCTION__ << ": global_ptr is " << GlobalClass::instance()->get_value() << std::endl;
+}
+
+void bar(void){
+	GlobalClass::instance()->set_value(2);
+	std::cout << __FUNCTION__ << ": global_ptr is " << GlobalClass::instance()->get_value() << std::endl;
+}
+
+int main()
+{
+	std::cout << __FUNCTION__ << ": global_ptr is " << GlobalClass::instance()->get_value() << std::endl;
+	foo();
+	bar();
+
+	return 0;
+}
