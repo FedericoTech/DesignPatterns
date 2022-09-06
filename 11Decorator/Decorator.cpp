676b6005123890dc4e74549b3f71daf06b23fb4e
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2022-09-06 16:41:57 +0100

    Decorator structural pattern

---
+++ 11Decorator/Decorator.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+//Step 1
+struct Shape {
+	virtual void draw() = 0;
+	virtual ~Shape(){}
+};
+
+//Step 2
+class Rectangle: public Shape {
+	void draw() {
+		std::cout << "Shape: Rectangle" << std::endl;
+	}
+};
+
+class Circle: public Shape {
+	void draw() {
+		std::cout << "Shape: Circle" << std::endl;
+	}
+};
+
+//Step 3
+
+struct ShapeDecorator: public Shape {
+	ShapeDecorator(Shape *decoratedShape):decoratedShape(decoratedShape) {};
+	void draw(){
+		decoratedShape->draw();
+	}
+	virtual ~ShapeDecorator() = 0; //to keep this class abstract as well
+protected:
+	Shape *decoratedShape;
+};
+
+ShapeDecorator::~ShapeDecorator(){}; //to keep this class abstract as well
+
+
+//Step 4
+
+class RedShapeDecorator: public ShapeDecorator {
+	void setRedBorder(Shape *decoratedShape){
+		std::cout << "Border Color: Red" << std::endl;
+	}
+public:
+	RedShapeDecorator(Shape *decoratedShape):ShapeDecorator(decoratedShape){};
+
+	void draw() {
+		decoratedShape->draw();
+		setRedBorder(decoratedShape);
+	}
+};
+
+//Step 5
+
+int main(){
+
+	Shape *circle = new Circle();
+
+	Shape *redCircle = new RedShapeDecorator(new Circle());
+
+	Shape *redRectangle = new RedShapeDecorator(new Rectangle());
+	std::cout << "Circle with normal border" << std::endl;
+	circle->draw();
+
+	std::cout << std::endl;
+
+	std::cout << "Circle of red border" << std::endl;
+	redCircle->draw();
+
+	std::cout << std::endl;
+
+	std::cout << "Rectangle of red border" << std::endl;
+	redRectangle->draw();
+
+	return 0;
+}
