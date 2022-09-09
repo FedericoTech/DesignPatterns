63c6076a2612bb71c92c4e7f5ea25eb102bab6fa
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2022-09-09 21:51:37 +0100

    Template behavioral pattern

---
+++ 25Template/Template.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+//Step 1
+struct Game
+{
+	virtual void initialize() = 0;
+	virtual void startPlay() = 0;
+	virtual void endPlay() = 0;
+	virtual ~Game() = 0;
+
+	//template method
+	virtual void play() final {
+		//initialize the game
+		initialize();
+
+		//start game
+		startPlay();
+
+		//end game
+		endPlay();
+	}
+};
+
+Game::~Game(){};
+
+//Step 2
+class Cricket: public Game
+{
+public:
+	void endPlay() override {
+		std::cout << "Cricket Game Finished!" << std::endl;
+	}
+
+	void initialize() override {
+		std::cout << "Cricket Game initialized! Start playing." << std::endl;
+	}
+
+	void startPlay() override {
+		std::cout << "Cricket Game Started. Enjoy the game!" << std::endl;
+	}
+};
+
+class Football: public Game
+{
+public:
+	void endPlay() override {
+		std::cout << "Football Game Finished!" << std::endl;
+	}
+
+	void initialize() override {
+		std::cout << "Football Game initialized! Start playing." << std::endl;
+	}
+
+	void startPlay() override {
+		std::cout << "Football Game Started. Enjoy the game!" << std::endl;
+	}
+};
+
+//Step 3
+
+int main(int argc, char *argv[])
+{
+	Game *game = new Cricket();
+	game->play();
+
+	delete game;
+
+	std::cout << std::endl;
+
+	game = new Football();
+	game->play();
+
+	delete game;
+
+	return 0;
+}
