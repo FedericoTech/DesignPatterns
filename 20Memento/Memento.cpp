7b3b78f1cdb1ef92ede51ddde65ba5f09f1f2ab0
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2022-09-09 17:17:58 +0100

    Memento behavioral pattern

---
+++ 20Memento/Memento.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+//Step 1
+class Memento
+{
+	std::string state;
+
+public:
+	Memento(std::string state):state(state){};
+
+	std::string getState()
+	{
+		return state;
+	}
+};
+
+//Step 2
+class Originator
+{
+	std::string state;
+
+public:
+	void setState(std::string state)
+	{
+		this->state = state;
+	}
+
+	std::string getState()
+	{
+		return state;
+	}
+
+	Memento *saveStateToMemento()
+	{
+		return new Memento(state);
+	}
+
+	void getStateFromMemento(Memento *memento)
+	{
+		state = memento->getState();
+	}
+};
+
+//State 3
+class CareTaker
+{
+	std::list<Memento *> mementoList;
+
+public:
+	void add(Memento *state){
+		mementoList.push_back(state);
+	}
+
+	Memento *get(int index)
+	{
+		auto l_front = mementoList.begin();
+		std::advance(l_front, index);
+		return *l_front;
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	Originator originator;
+	CareTaker careTaker;
+
+	originator.setState("State #1");
+	originator.setState("State #2");
+
+	careTaker.add(originator.saveStateToMemento());
+
+	originator.setState("State #3");
+	careTaker.add(originator.saveStateToMemento());
+
+	originator.setState("state #4");
+	std::cout << "Current State: " << originator.getState() << std::endl;
+
+	originator.getStateFromMemento(careTaker.get(0));
+	std::cout << "First saved State: " << originator.getState() << std::endl;
+
+	originator.getStateFromMemento(careTaker.get(1));
+	std::cout << "Second saved State: " << originator.getState() << std::endl;
+
+	return 0;
+}
