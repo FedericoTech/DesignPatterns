e5c36baa5713c667ad3596a4bc6c2dea0faa800d
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2022-09-15 14:56:34 +0100

    Singleton pattern

--- 06Singleton/Singleton.cpp
+++ 06Singleton/Singleton.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
+#include <memory>
 
 class GlobalClass
 {
 	int m_value;
-	static GlobalClass *s_instance;
+
+	static std::shared_ptr<GlobalClass> s_instance;
 
 	GlobalClass(int v = 0) //we make the constructor private
 	{
 		m_value = v;
 	}
 
-	void operator = (GlobalClass const&) = delete;	//we get rid of the copy constructor
+	GlobalClass(GlobalClass const &) = delete; //we get rid of the copy constructor
+
+	GlobalClass& operator=(GlobalClass const &) = delete;	//we get rid of the copy assignation
 
 public:
 	int get_value(){ return m_value; }
 
 	void set_value(int v) { m_value = v;}
 
-	static GlobalClass *instance()
+	static std::shared_ptr<GlobalClass> instance()
 	{
-		if(!s_instance){
-			s_instance = new GlobalClass;
+		if(s_instance == nullptr){
+			s_instance = std::move(std::shared_ptr<GlobalClass>(new GlobalClass));
 		}
 
 		return s_instance;
 	}
 };
 
-GlobalClass *GlobalClass::s_instance = 0;
+std::shared_ptr<GlobalClass> GlobalClass::s_instance = nullptr;
 
 void foo(void){
 	GlobalClass::instance()->set_value(1);
@@ -41,6 +45,12 @@ void bar(void){
 
 int main()
 {
+	//we can get raw pointers from the instance
+	GlobalClass *ello = GlobalClass::instance().get();
+	GlobalClass *ello2 = GlobalClass::instance().get();
+	//and the raw pointers point to the same instance
+	std::cout << "ello == ello2 ? " << (ello == ello2) << std::endl;
+
 	std::cout << __FUNCTION__ << ": global_ptr is " << GlobalClass::instance()->get_value() << std::endl;
 	foo();
 	bar();
